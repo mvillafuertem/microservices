4c4305b73520698ef020e04e2b65d52bf2d786cf
Miguel Villafuerte <mivimur@hotmail.com>
2018-02-08 20:44:43 +0100

    initial commit

---
+++ src/main/java/com/mvillafuertem/usermicroservice/UsermicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.mvillafuertem.usermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermicroserviceApplication.class, args);
+	}
+}
